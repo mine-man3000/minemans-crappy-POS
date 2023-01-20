123cf04ffe86d400280b466c55555a6c4b87c3b3
Autumn Williams <brandonhdyc@gmail.com>
2023-01-20 15:22:14 -0500

    made documentation

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const { app, BrowserWindow } = require('electron')
 var ipc = require('electron').ipcRenderer;
+//load fs
 const fs = require('fs')
+//set up the launch mode paramater
 var launchmode = JSON.parse(fs.readFileSync('config/config.json'));
-
+//reads if the launch paramater is run
 if(launchmode.launchin === "run") {
 console.log("loading in standard mode....")
 app.whenReady().then(() => {
@@ -13,6 +15,7 @@ app.whenReady().then(() => {
 	win.loadFile('index.html')
 })
 }
+//reads if launch paramater is service
 if(launchmode.launchin === "service") {
 	app.whenReady().then(() => {
 		const win = new BrowserWindow({
