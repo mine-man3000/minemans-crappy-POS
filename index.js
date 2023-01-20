bab3d93414c5accbaa6e27f1461e823deec8b6e6
Autumn Williams <brandonhdyc@gmail.com>
2023-01-20 00:20:01 -0500

    Made it look ugly as shit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { app, BrowserWindow } = require('electron')
 var ipc = require('electron').ipcRenderer;
-
+const fs = require('fs')
 
 app.whenReady().then(() => {
 	const win = new BrowserWindow({
