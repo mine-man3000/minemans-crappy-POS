adddebeb1a7649195b0196cfb92edbac99fb362b
Autumn Williams <brandonhdyc@gmail.com>
2023-01-20 15:58:48 -0500

    takes in theme setting from JSON, does not work yet

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const { app, BrowserWindow } = require('electron')
 var ipc = require('electron').ipcRenderer;
 //load fs
 const fs = require('fs')
-//set up the launch mode paramater
-var launchmode = JSON.parse(fs.readFileSync('config/config.json'));
+//set up the config list
+var config = JSON.parse(fs.readFileSync('config/config.json'));
+module.exports.config = config
 //reads if the launch paramater is run
-if(launchmode.launchin === "run") {
+if(config.launchin === "run") {
 console.log("loading in standard mode....")
 app.whenReady().then(() => {
 	const win = new BrowserWindow({
@@ -13,10 +14,11 @@ app.whenReady().then(() => {
 		height: 600
     })
 	win.loadFile('index.html')
+	
 })
 }
 //reads if launch paramater is service
-if(launchmode.launchin === "service") {
+if(config.launchin === "service") {
 	app.whenReady().then(() => {
 		const win = new BrowserWindow({
 			width: 800,
